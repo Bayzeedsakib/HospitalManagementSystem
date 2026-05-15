b2f7b488205c3a7b4eb62099e6d9b18a87de4b33
Bayzeedsakib <angelsakib4@gmail.com>
2026-05-16 02:17:30 +0600

    started Authentication structure part and add necessary files

---
+++ BLL/DTOs/UserDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        public string Password { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
+        public string? RoleName { get; set; }
+
+
+    }
+}
---
+++ Hospital.Web/Controllers/AuthController.cs
@@ -0,0 +1,6 @@
+﻿namespace Hospital.Web.Controllers
+{
+    public class AuthController
+    {
+    }
+}
